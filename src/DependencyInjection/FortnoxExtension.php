9ee83c6d033499a8af497a65fb9e0950f9e6a49f
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 12:46:38 +0100

    Update FortnoxExtension.php

--- src/DependencyInjection/FortnoxExtension.php
+++ src/DependencyInjection/FortnoxExtension.php
@@ -4,7 +4,7 @@ namespace ITBMedia\FortnoxBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\Config\FileLocator;
+use Symfony\Component\Config\FileLocator;
 
 class FortnoxExtension extends Extension
 {
