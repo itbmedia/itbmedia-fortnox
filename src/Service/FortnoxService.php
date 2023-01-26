20380c55c8f07f446ecb9de4d55d2ff5b36d1b45
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 19:14:44 +0100

    Update FortnoxService.php

--- src/Service/FortnoxService.php
+++ src/Service/FortnoxService.php
@@ -29,7 +29,7 @@ class FortnoxService{
     {
         $articles = array();
         $response = $this->call($token, 'GET', 'articles', $params);
-        print_r(get_class($response)):
+        print_r(get_class($response));
         die;
         foreach($response['Articles'] as $article)
         {
