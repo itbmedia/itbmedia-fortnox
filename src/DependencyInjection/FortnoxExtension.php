185f565b55fdaf77ac5969e7033f0b2cd9fee894
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 13:00:42 +0100

    Update FortnoxExtension.php

--- src/DependencyInjection/FortnoxExtension.php
+++ src/DependencyInjection/FortnoxExtension.php
@@ -17,7 +17,7 @@ class FortnoxExtension extends Extension
 	 * @return mixed
 	 */
 	public function load(array $configs, ContainerBuilder $container) {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
 
         $configuration = new Configuration();
