89add504d43acc22917ff6c583b11b5670dd4938
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 13:25:27 +0100

    Update FortnoxService.php

--- src/Service/FortnoxService.php
+++ src/Service/FortnoxService.php
@@ -1,6 +1,7 @@
 <?php
 namespace ITBMedia\FortnoxBundle\Service;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class FortnoxService{
 
@@ -9,5 +10,19 @@ class FortnoxService{
      */
     public function __construct(ParameterBagInterface $parameterBag) {
         $this->parameterBag = $parameterBag;
+    }
+
+    public function initializeAuth(string $callback, string $type = "code", array $scopes = [], array $state = []) : RedirectResponse
+    {
+        return new RedirectResponse("https://apps.fortnox.se/oauth-v1/auth?" . http_build_query(
+                array(
+                    'client_id' => $this->parameterBag->get('fortnox_bundle.client_id'),
+                    'redirect_uri' => $callback,
+                    'response_type' => $type,
+                    'scope' => implode($scopes, ' '),
+                    'state' => $state,
+                )
+            )
+        );
     }
 }
