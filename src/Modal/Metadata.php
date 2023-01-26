5e910ad6541ff529c320e38dac86375da386980b
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 19:28:32 +0100

    Update Metadata.php

--- src/Modal/Metadata.php
+++ src/Modal/Metadata.php
@@ -8,5 +8,91 @@ use JMS\Serializer\Annotation\Type;
 
 class Metadata implements SerializableInterface
 {
-    
+     /**
+     * @var int
+     * @Type("integer")
+     * @SerializedName("@TotalResources")
+     */
+    private $totalResources;
+     /**
+     * @var int
+     * @Type("integer")
+     * @SerializedName("@TotalPages")
+     */
+    private $totalPages;
+     /**
+     * @var int
+     * @Type("integer")
+     * @SerializedName("@CurrentPage")
+     */
+    private $currentPage;
+
+
+	/**
+	 * 
+	 * @return int
+	 */
+	public function getTotalResources() {
+		return $this->totalResources;
+	}
+	
+	/**
+	 * 
+	 * @param int $totalResources 
+	 * @return self
+	 */
+	public function setTotalResources($totalResources): self {
+		$this->totalResources = $totalResources;
+		return $this;
+	}
+	
+	/**
+	 * 
+	 * @return int
+	 */
+	public function getTotalPages() {
+		return $this->totalPages;
+	}
+	
+	/**
+	 * 
+	 * @param int $totalPages 
+	 * @return self
+	 */
+	public function setTotalPages($totalPages): self {
+		$this->totalPages = $totalPages;
+		return $this;
+	}
+	
+	/**
+	 * 
+	 * @return int
+	 */
+	public function getCurrentPage() {
+		return $this->currentPage;
+	}
+	
+	/**
+	 * 
+	 * @param int $currentPage 
+	 * @return self
+	 */
+	public function setCurrentPage($currentPage): self {
+		$this->currentPage = $currentPage;
+		return $this;
+	}
+
+    /**
+	 * @return string
+	 */
+	public function serialize() {
+		return SerializerBuilder::create()->build()->serialize($this, 'json');
+	}
+	
+	/**
+	 * @return Metadata
+	 */
+	public static function deserialize(string $data) {
+		return SerializerBuilder::create()->build()->deserialize($data, self::class, 'json');
+	}
 }
