e85d1d939d85a058a808b5accd86370552063e3f
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 19:17:32 +0100

    Update FortnoxService.php

--- src/Service/FortnoxService.php
+++ src/Service/FortnoxService.php
@@ -29,12 +29,13 @@ class FortnoxService{
     {
         $articles = array();
         $response = $this->call($token, 'GET', 'articles', $params);
-        print_r($response['Articles']);
-        die;
         foreach($response['Articles'] as $article)
         {
-            $articles[] = Article::deserialize(json_encode($article));
+            print_r($article);
+            // $articles[] = Article::deserialize(json_encode($article));
         }
+        die;
+
         return $articles;
     }
 
