b5b28269f41962b3bf8102164be7300c33af35ad
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 14:09:14 +0100

    redirect url added

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -22,6 +22,7 @@ class Configuration implements ConfigurationInterface{
             ->children()
                 ->scalarNode('client_id')->end()
                 ->scalarNode('client_secret')->end()
+                ->scalarNode('redirect_url')->end()
             ->end();
 
         return $treeBuilder;
