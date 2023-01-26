61d14ad3954feb552d642aeede56949036eff383
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 15:55:14 +0100

    Update Token.php

--- src/Model/Token.php
+++ src/Model/Token.php
@@ -1,5 +1,7 @@
 <?php
 namespace ITBMedia\FortnoxBundle\Modal;
+
+use ITBMedia\FortnoxBundle\Modal\SerializableInterface;
 use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\Annotation\SerializedName;
 use JMS\Serializer\Annotation\Type;
