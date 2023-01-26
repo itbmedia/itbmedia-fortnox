d8c0586fce4d3a15b8f9f7b7c9700d8d41929779
LucasNapik98 <lucas.napik@outlook.com>
2023-01-26 14:37:22 +0100

    rest

---
+++ src/Controller/CallbackController.php
@@ -0,0 +1,28 @@
+<?php
+namespace ITBMedia\FortnoxBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CallbackController extends Controller
+{
+    private ParameterBagInterface $parameterBag;
+    private EventDispatcherInterface $eventDispatcher;
+
+    public function __construct(ParameterBagInterface $parameterBag, EventDispatcherInterface $eventDispatcher)
+    {
+        $this->parameterBag = $parameterBag;
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    public function fortnoxCallback(Request $request)
+    {
+        return $this->redirect($this->parameterBag->get('fortnox_bundle.callback_url').http_build_query(
+            $res
+        ));
+    }
+}
