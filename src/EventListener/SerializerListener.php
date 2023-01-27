506ba7942ec4121fdc01d6c668d6f57367eb83e4
LucasNapik98 <lucas.napik@outlook.com>
2023-01-27 10:38:27 +0100

    a

--- src/EventListener/SerializerListener.php
+++ src/EventListener/SerializerListener.php
@@ -4,21 +4,8 @@ use ITBMedia\FortnoxBundle\Event\ConnectEvent;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
 use JMS\Serializer\EventDispatcher\PreSerializeEvent;
 
-class SerializerListener implements EventSubscriberInterface
+class SerializerListener
 {
-    public static function getSubscribedEvents()
-    {
-        return array(
-            array(
-                'event' => 'serializer.pre_serialize',
-                'method' => 'onPreSerialize',
-                // 'class' => 'AppBundle\\Entity\\SpecificClass', // if no class, subscribe to every serialization
-                // 'format' => 'json', // optional format
-                'priority' => 0, // optional priority
-            ),
-        );
-    }
-
     public function onPreSerialize(PreSerializeEvent $event)
     {
         die("ok");
