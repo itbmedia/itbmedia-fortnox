a513012e18a36d511b90cf03b017c83b22c360d6
Mambans <perssons1996@gmail.com>
2025-11-14 19:33:05 +0100

    itbmedia/kund24#4661

--- src/Model/PrintTemplate.php
+++ src/Model/PrintTemplate.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ITBMedia\FortnoxBundle\Model;
 
 use ITBMedia\FortnoxBundle\Model\SerializableInterface;
@@ -8,45 +9,89 @@ use JMS\Serializer\Annotation\Type;
 
 class PrintTemplate implements SerializableInterface
 {
-    /**
-	* @var string
-	* @Type("string")
-	* @SerializedName("Template")
-	*/
-	private $template;
-    
 	/**
-	* @var string
-	* @Type("string")
-	* @SerializedName("Name")
-	*/
+	 * @var string
+	 * @Type("string")
+	 * @SerializedName("Template")
+	 */
+	private $template;
+
+	/**
+	 * @var string
+	 * @Type("string")
+	 * @SerializedName("Name")
+	 */
 	private $name;
 
-    /**
+	/**
+	 * @return string|null
+	 */
+	public function getTemplate(): ?string
+	{
+		return $this->template;
+	}
+
+	/**
+	 * @param string|null $template
+	 *
+	 * @return PrintTemplate
+	 */
+	public function setTemplate(?string $template): self
+	{
+		$this->template = $template;
+
+		return $this;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getName(): ?string
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @param string|null $name
+	 *
+	 * @return PrintTemplate
+	 */
+	public function setName(?string $name): self
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
 	 * @return string
 	 */
-	public function serialize() {
+	public function serialize()
+	{
 		return SerializerBuilder::create()->build()->serialize($this, 'json');
 	}
-	
+
 	/**
 	 * @return PrintTemplate
 	 */
-	public static function deserialize(string $data) {
+	public static function deserialize(string $data)
+	{
 		return SerializerBuilder::create()->build()->deserialize($data, self::class, 'json');
 	}
 
 	/**
 	 * @return array
 	 */
-	public function toArray() : array {
+	public function toArray(): array
+	{
 		return SerializerBuilder::create()->build()->toArray($this);
 	}
 
 	/**
 	 * @return PrintTemplate
 	 */
-	public static function fromArray(array $data) {
+	public static function fromArray(array $data)
+	{
 		return SerializerBuilder::create()->build()->fromArray($data, self::class);
 	}
 }
